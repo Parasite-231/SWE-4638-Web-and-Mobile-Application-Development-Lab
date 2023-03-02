01a833966ed70a454d86eb81ef952338271507de
Parasite-231 <muktadir@iut-dhaka.edu>
2023-03-02 23:13:28 +0600

    lab-03

---
+++ client/src/components/BookListLayout.js
@@ -0,0 +1,83 @@
+
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
+
+export default function BookListLayout() {
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [booksPerPage, setBooksPerPage] = useState(10);
+  const [bookList, setBookList] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/bookList").then((response) => {
+      setBookList(response.data);
+    });
+  }, []);
+
+  const indexOfLastBook = currentPage * booksPerPage;
+  const indexOfFirstBook = indexOfLastBook - booksPerPage;
+  const currentBooks = bookList.slice(indexOfFirstBook, indexOfLastBook);
+
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(bookList.length / booksPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  const deleteBook = (name) => {
+    axios.delete(`http://localhost:3001/delete/${name}`).then((response) => {
+      // update the bookList state after deleting the book
+      setBookList(bookList.filter(book => book.name !== name));
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+
+  return (
+    <div>
+      <h2 style={{ textAlign: "center" }}>Book List</h2>
+      <table class="table" style={{ margin: "5%" }}>
+        <thead>
+          <tr>
+            <th scope="col">#SL</th>
+            <th scope="col">Name</th>
+            <th scope="col">Author</th>
+            <th scope="col">Genre</th>
+            <th scope="col-2" style={{ textAlign: "center" }}>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentBooks.map((book) => {
+            return (
+              <tr key={book.id}>
+                <th scope="row">{book.id}</th>
+                <td>{book.name}</td>
+                <td>{book.author}</td>
+                <td>{book.genre}</td>
+                <td><NavLink to={`/${book.id}'`}><button type="button" className="btn btn-success">Update</button></NavLink></td>
+                <td><button  onClick={()=> {deleteBook(book.name)}}  className="btn btn-danger">Delete</button></td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+      <div class="container " style={{ marginBottom: "10%" }}>
+        <div className="d-flex justify-content-center">
+          <nav>
+            <ul className="pagination">
+              {pageNumbers.map((number) => (
+                <li key={number} className="page-item">
+                  <button onClick={() => setCurrentPage(number)} className="btn btn-outline-dark" style={{marginRight:"10px"}}>
+                    {number}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </div>
+        <button type="submit" class="btn btn-primary " style={{ marginRight: "4%" }}>Show more</button>
+        <NavLink to="/"><button type="submit" class="btn btn-info">Add more</button></NavLink>
+      </div>
+    </div>
+  )
+}
